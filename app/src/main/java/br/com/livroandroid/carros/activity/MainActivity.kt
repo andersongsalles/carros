f52202c13e665ad4fff9d3167e6eebc04f3c2722
Anderson Home <agsalles@gmail.com>
2017-09-02 20:09:06 -0300

    Insert Activity Extensions, inserindo toolbar na MainActivity

--- app/src/main/java/br/com/livroandroid/carros/activity/MainActivity.kt
+++ app/src/main/java/br/com/livroandroid/carros/activity/MainActivity.kt
@@ -3,11 +3,13 @@ package br.com.livroandroid.carros.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.livroandroid.carros.R
+import br.com.livroandroid.carros.extensions.setupToolbar
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar(R.id.toolbar);
     }
 }
